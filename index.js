a9ccfc1e94b9d8ab153b6e79417f40cf9b3aa6e9
Roman <ayoz4@yandex.ru>
2021-07-13 13:31:15 +0700

    js done

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
-$(document).ready(function () {
-  $('.sidebar__burger').click(function (event) {
-    $('.sidebar__burger,.sidebar,.sidebar__btn,.sidebar__logo,.sidebar__list').toggleClass(
-      'active',
-    );
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var btn = document.querySelector(".sidebar__burger");
 
-  $(window).scroll(function () {
-    let width = $(window).width();
+  btn.onclick = function () {
+    var tests = document.querySelectorAll(
+      ".sidebar__burger,.sidebar,.sidebar__btn,.sidebar__logo,.sidebar__list"
+    );
+
+    var isActive = false;
+
+    if (tests[0].classList[1]) {
+      isActive = true;
+    }
+
+    for (let index = 0; index < tests.length; index++) {
+      if (isActive) tests[index].classList.remove("active");
+      else tests[index].classList.add("active");
+    }
+  };
+
+  window.addEventListener("scroll", function () {
+    var width = document.documentElement.clientWidth;
 
     if (width < 769) {
-      if ($('html,body').scrollTop() > 27 && width > 321) {
-        $('.header__fixed').css({
-          'padding-left': '77px',
-          top: '0',
-          position: 'fixed',
-          width: 'calc(100% - 77px)',
-        });
-        $('.header__input').css({
-          width: 'calc(100% - 77px)',
-        });
-      } else if (width < 321 && $('html,body').scrollTop() > 27) {
-        $('.header__fixed').css({ 'padding-left': '68px', transition: 'all 0.3s' });
-        $('.header__input').css({ background: 'none', 'padding-left': '16px' });
+      var header = document.querySelector(".header__fixed");
+      var headerInput = document.querySelector(".header__input");
+
+      if (document.querySelectorAll("html")[0].scrollTop > 27 && width > 321) {
+        header.style.paddingLeft = "77px";
+        header.style.position = "fixed";
+        header.style.top = "0";
+        header.style.width = "calc(100% - 77px)";
+
+        headerInput.style.width = "calc(100% - 77px)";
+      } else if (
+        width < 321 &&
+        document.querySelectorAll("html")[0].scrollTop > 27
+      ) {
+        header.style.paddingLeft = "68px";
+        header.style.transition = "all 0.3s";
+
+        headerInput.style.background = "none";
+        headerInput.style.paddingLeft = "16px";
       } else {
-        $('.header__fixed').css({ 'padding-left': '26px', position: 'relative', width: '100%' });
-        $('.header__input').css({
-          background: 'url(header/search.svg) no-repeat scroll 16px 16px',
-          width: '100%',
-        });
+        header.style.paddingLeft = "26px";
+        header.style.position = "relative";
+        header.style.width = "100%";
+
+        headerInput.style.background =
+          "url(header/search.svg) no-repeat scroll 16px 16px";
+        headerInput.style.width = "100%";
       }
     }
 
     if (width >= 1440) {
-      $('.header').css('padding-left', '60px');
+      document.querySelector(".header").style.paddingLeft = "60px";
     }
   });
 });
